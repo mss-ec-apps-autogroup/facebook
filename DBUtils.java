23b424cce8ad1d0832f5171355084f18b39d109c
vamsi reddy <rvamshi1998@gmail.com>
2021-12-19 12:31:47 +0530

    updating DBUtils.java file

--- DBUtils.java
+++ DBUtils.java
@@ -2,9 +2,4 @@ hi
 This line is updatd
 This line is updated again
 
-this line is updating in master branch
 
-again uodating
-this line is updating 2nd time
-
-this line is updating in development branch
